40e442d8bb11b1e4ab5cfb62c0324d5ad1f2c394
Marc Mendoza <777marc@gmail.com>
2025-08-20 17:47:05 -0400

    that's it for today

--- recipes/auth/app.py
+++ recipes/auth/app.py
@@ -2,11 +2,10 @@ import functools
 
 from flask_login import login_user, logout_user, current_user, login_required
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from recipes.auth.models import User
-from recipes.app import bcrypt
 
 bp = Blueprint('auth', __name__, url_prefix='/auth', template_folder='templates')
 
@@ -16,12 +15,12 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         user = User.query.filter_by(username=username).first()
-        if user and bcrypt.check_password_hash(user.password, password):
+        if user and  current_app.extensions['bcrypt'].check_password_hash(user.password, password):
             login_user(user)
             print(f"User {user.username} logged in")
             return redirect(url_for('index'))
         
-    return render_template('login.html')
+    return render_template('auth/login.html')
 
 
 @bp.route('/register')
